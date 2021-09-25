e476d92ea35a404dae4d8e4d712ef4df133e1175
victordev13 <vtrcarvalho.13@gmail.com>
2021-09-25 13:12:53 -0300

    feat: página de post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
 import styles from './header.module.scss';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <img src="/spacetraveling.svg" alt="logo" />
+        <Link href="/">
+          <a>
+            <img src="/spacetraveling.svg" alt="logo" />
+          </a>
+        </Link>
       </div>
     </header>
   );
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,12 +1,15 @@
+import format from 'date-fns/format';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { RichText } from 'prismic-dom';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
-interface Post {
+type Post = {
   first_publication_date: string | null;
   data: {
     title: string;
@@ -21,15 +24,49 @@ interface Post {
       }[];
     }[];
   };
-}
+};
 
 interface PostProps {
   post: Post;
 }
 
-export default function Post({}: PostProps) {
-  // TODO
-  return <div></div>;
+export default function Post({ post }: PostProps) {
+  return (
+    <main>
+      <header className={styles.header}>
+        <img src={post.data.banner.url} />
+        <div>
+          <h1>{post.data.title}</h1>
+          <div className={styles.postMeta}>
+            <div>
+              <FiCalendar />
+              <time>{post.first_publication_date}</time>
+            </div>
+            <div>
+              <FiUser />
+              <span>{post.data.author}</span>
+            </div>
+            <div>
+              <FiClock />
+              <span>4min</span>
+            </div>
+          </div>
+        </div>
+      </header>
+      <article className={styles.post}>
+        {post.data.content.map((content, key) => {
+          return (
+            <div key={key} className={styles.postContent}>
+              <h2>{content.heading}</h2>
+              {content.body.map((body, key) => {
+                return <p key={key}>{body.text}</p>;
+              })}
+            </div>
+          );
+        })}
+      </article>
+    </main>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -48,29 +85,34 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug } = params;
 
   const response = await prismic.getByUID('post', String(slug), {});
-  console.log(response);
-  // const post: Post = {
-  //   first_publication_date: '',
-  //   data: {
-  //     title: RichText.asText(response.data.title),
-  //     banner: {
-  //       url: 'string',
-  //     },
-  //     author: RichText.asText(response.data.author),
-  //     content: [
-  //       {
-  //         heading: 'string',
-  //         body: [
-  //           {
-  //             text: 'string',
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   },
-  // };
+  const post: Post = {
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      'dd LLL yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content?.map(content => {
+        return {
+          heading: content.heading,
+          body: content.body.map(body => {
+            return {
+              text: body.text,
+            };
+          }),
+        };
+      }),
+    },
+  };
 
   return {
-    props: {},
+    props: { post },
   };
 };
